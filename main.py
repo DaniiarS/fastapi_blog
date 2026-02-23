4e1625e3c9ec672a32812fd9693f66859880a1dd
Daniiar Suiunbekov <daniyar.suyunbekov@gmail.com>
2026-02-23 16:32:05 +0600

    Lesson 6 - CRUD Operations (PUT, PATCH, DELETE)

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ from sqlalchemy.orm import Session
 import models
 from database import engine, get_db, Base
 
-# This is executed on start. Creates tables if they don't exist in the data base, otherwise does nothing
-# It safe to run it multiple times
+# This is executed on start. Creates tables if they don't exist in the database, otherwise does nothing
+# It is safe to run it multiple times
 Base.metadata.create_all(bind=engine)
 
 # Pydantic models
-from schemas import PostCreate, PostResponse, UserCreate, UserResponse
+from schemas import PostCreate, PostResponse, UserCreate, UserResponse, PostUpdate, UserUpdate
 
 # Change the decorator on home by giving each decorator a name (ex. "home", "posts") so that it does correct routing
 
@@ -76,29 +76,8 @@ def user_posts_page(request: Request, user_id: int, db: Annotated[Session, Depen
 # API Routes: Post and User
 # ========================================================================
 
-@app.post("/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(select(models.User).where(models.User.username == user.username))
-    existing_user = result.scalars().first()
-
-    if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exist")
-    
-    result = db.execute(select(models.User).where(models.User.email == user.email))
-    existing_email = result.scalars().first()
-
-    if existing_email:
-        raise HTTPException(status=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
-    
-    new_user = models.User(username=user.username, email=user.email)
-
-    # Note: ask chatGPT about these commands
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
+#------------------------------------------- USER -------------------------------------------
+#--------------------------------------------------------------------------------------------
 @app.get("/api/users/{user_id}", response_model=UserResponse)
 def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.User).where(models.User.id == user_id))
@@ -131,6 +110,84 @@ def get_users(db: Annotated[Session, Depends(get_db)]):
 
     return users
 
+@app.post("/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.username == user.username))
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exist")
+    
+    result = db.execute(select(models.User).where(models.User.email == user.email))
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(status=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
+    
+    new_user = models.User(username=user.username, email=user.email)
+
+    # Note: ask chatGPT about these commands
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+# PATCH method - updates information partially (some fields)
+# PUT method - updates information fully (replaces old item with new)
+@app.patch("/api/users/{user_id}", response_model=UserResponse)
+def update_user_partial(user_id: int, user_update: UserUpdate, db: Annotated[Session, Depends(get_db)]):
+    # check is the user to be updated exists
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    # if the user exists, check if the value to be updated is not the same as it already is
+    if user_update.username is not None and user_update.username != user.username:
+        # check if the new_username is not taken already
+        result = db.execute(select(models.User).where(models.User.username == user_update.username))
+        existing_user = result.scalars().first()
+
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+        
+    # Repeat for the email field
+    if user.email is not None and user.email != user_update.email:
+        result = db.execute(select(models.User).where(models.User.email == user_update.email))
+        existing_user = result.scalars().first()
+
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+    
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    # check if the user to delete exists in the database
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User doesn't exist")
+    
+    db.delete(user)
+    db.commit()
+
+
+#------------------------------------------- POST -------------------------------------------
+#--------------------------------------------------------------------------------------------
 @app.get('/api/posts', response_model=list[PostResponse])
 def get_posts(db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.Post))
@@ -147,6 +204,64 @@ def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     
     return post
+
+@app.put("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_full(post_id: int, post_data: PostCreate, db: Annotated[Session, Depends(get_db)]):
+    # check if the post that needs to be changed exists
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    # check if the user requesting an update exists
+    result = db.execute(select(models.User).where(models.User.id == post_data.user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
+
+    db.commit()
+    db.refresh(post)
+
+    return post
+
+@app.patch("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_partial(post_id: int, post_data: PostUpdate, db: Annotated[Session, Depends(get_db)]):
+    # check if the post to be updated exists
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    # this line excludes fields in the request which have None values
+    update_data = post_data.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(post, key, value)
+    
+    db.commit()
+    db.refresh(post)
+
+    return post
+
+# DELETE Methods doesn't have response body, instead it returns HTTP 204 code, which means that
+# request was successfull, and deletion took place
+@app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    db.delete(post)
+    db.commit()
+    
 
 @app.post('/api/posts', response_model=PostResponse, status_code=status.HTTP_201_CREATED)
 def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
